f5d931039d60300532d9cd93650d0f34b727c902
Frederick-S <mao_xiaodan@hotmail.com>
2014-11-13 00:23:04 +0000

    Update test

--- test.js
+++ test.js
@@ -1,20 +1,28 @@
 var assert = require('assert');
-var gcd = require('./lib/gcd.js').gcd;
+var gcd = require('./lib/gcd.js');
 
 describe('GCD', function () {
-    it('2', function () {
+    it('The greatest common divisor of 0 and 2 is 2', function () {
         assert.equal(2, gcd(0, 2));
     });
     
-    it('4', function () {
+    it('The greatest common divisor of 8 and 12 is 4', function () {
         assert.equal(4, gcd(8, 12));
     });
     
-    it('1', function () {
+    it('The greatest common divisor of of 3 and 2 is 1', function () {
         assert.equal(1, gcd(3, 2));
     });
     
-    it('6', function () {
+    it('The greatest common divisor of of 24 and 18 is 6', function () {
         assert.equal(6, gcd(24, 18));
+    });
+    
+    it('The greatest common divisor of 1, 2 and 3 is 1', function () {
+        assert.equal(1, gcd(1, 2, 3));
+    });
+    
+    it('The greatest common divisor of 12, 15 and 21 is 3', function () {
+        assert.equal(3, gcd(12, 15, 21));
     });
 });
